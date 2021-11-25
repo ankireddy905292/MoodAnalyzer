31e22f097726fdd53024c416aca725ddfcca7cb8
Tutorials-Git <ankireddy905292@gmail.com>
2021-11-25 11:26:25 +0530

    Mood Analyzer Handle

--- src/com/moodanalyzer/MoodAnalyzer.java
+++ src/com/moodanalyzer/MoodAnalyzer.java
@@ -1,19 +1,23 @@
 package com.moodanalyzer;
 
 public class MoodAnalyzer {
-    public static void main(String[] args) {
 
-    }
     private String message;
     public MoodAnalyzer(String message) {
         this.message = message;
     }
 
     public String analyseMood() {
-        if(message.contains("Sad"))
-            return "SAD";
-        else
+        try {
+
+            if (this.message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (NullPointerException e) {
             return "HAPPY";
+        }
     }
 }
+
 
